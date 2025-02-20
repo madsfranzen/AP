c5fbd71cc363729a3bd40f002c43449d0d46a059
Mads Franzen <madsfranzen@live.dk>
2025-02-20 14:55:02 +0100

    Update Gradle configuration and clean up build artifacts

--- Lektion3/DictionaryBST.java
+++ Lektion3/DictionaryBST.java
@@ -1,5 +1,5 @@
 public class DictionaryBST<K extends Comparable<K>, V> implements
-Dictionary<K, V> {
+		Dictionary<K, V> {
 
 	private Node root;
 
@@ -7,11 +7,28 @@ Dictionary<K, V> {
 		root = null;
 	}
 
+	private class Node {
+		private K key;
+		private V value;
+		private Node left;
+		private Node right;
+
+		public Node(K key, V value) {
+			this.key = key;
+			this.value = value;
+			this.left = null;
+			this.right = null;
+		}
+	}
+
 	@Override
 	public V get(K key) {
-		// TODO
-		return null;
-
+		Node n = find(key);
+		if (n == null) {
+			return null;
+		} else {
+			return n.value;
+		}
 	}
 
 	private Node find(K key) {
@@ -32,48 +49,162 @@ Dictionary<K, V> {
 		} else {
 			return null;
 		}
-
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO
-		return false;
+		if (size() == 0) {
+			return true;
+		} else
+			return false;
 	}
 
+	/**
+	 * Adds a new key-value pair to the binary search tree or updates an existing
+	 * key's value
+	 * 
+	 * @param key   The key to add or update
+	 * @param value The value to associate with the key
+	 * @return The old value if key existed, null if it's a new entry
+	 */
 	@Override
 	public V put(K key, V value) {
-		// TODO
+		// If tree is empty, create root node
+		if (root == null) {
+			root = new Node(key, value);
+		} else {
+			// Otherwise, recursively insert into the tree
+			return put(key, value, root);
+		}
 		return null;
+	}
 
+	/**
+	 * Helper method that recursively traverses the tree to insert or update a node
+	 * 
+	 * @param key    The key to add or update
+	 * @param value  The value to associate with the key
+	 * @param parent The current node being examined
+	 * @return The old value if key existed, null if it's a new entry
+	 */
+	private V put(K key, V value, Node parent) {
+		// If key already exists, update the value and return old value
+		if (parent.key.equals(key)) {
+			V oldValue = parent.value;
+			parent.value = value;
+			return oldValue;
+
+		} else if (key.compareTo(parent.key) > 0) {
+			// If new key is greater than parent key, go to right subtree
+			if (parent.right == null) {
+				// If right child is empty, create new node here
+				parent.right = new Node(key, value);
+			} else {
+				// Otherwise, continue recursing down right subtree
+				put(key, value, parent.right);
+			}
+			return null;
+		} else {
+			// If new key is less than parent key, go to left subtree
+			if (parent.left == null) {
+				// If left child is empty, create new node here
+				parent.left = new Node(key, value);
+			} else {
+				// Otherwise, continue recursing down left subtree
+				put(key, value, parent.left);
+			}
+			return null;
+		}
 	}
 
 	@Override
 	public V remove(K key) {
-		// TODO
-		return null;
+		V removedV = null;
+		Node current = root;
+		Node parent = null;
+
+		// Find the node to remove
+		while (current != null && current.key != key) {
+			parent = current;
+			if (key.compareTo(current.key) < 0) {
+				current = current.left;
+			} else {
+				current = current.right;
+			}
+		}
+
+		// If the node doesn't exist, return null (no removal)
+		if (current == null) {
+			return removedV;
+		}
+
+		// Node to be removed is found
+		removedV = current.value;
+
+		// Case 1: Node has no left child
+		if (current.left == null) {
+			if (parent == null) {
+				// Removing the root node
+				root = current.right;
+			} else {
+				if (key.compareTo(parent.key) < 0) {
+					parent.left = current.right;
+				} else {
+					parent.right = current.right;
+				}
+			}
+		}
+		// Case 2: Node has a left child
+		else if (current.right == null) {
+			if (parent == null) {
+				// Removing the root node
+				root = current.left;
+			} else {
+				if (key.compareTo(parent.key) < 0) {
+					parent.left = current.left;
+				} else {
+					parent.right = current.left;
+				}
+			}
+		}
+		// Case 3: Node has both children
+		else {
+			// Find the rightmost node in the left subtree
+			Node parentOfRightMost = current;
+			Node rightMost = current.left;
+			while (rightMost.right != null) {
+				parentOfRightMost = rightMost;
+				rightMost = rightMost.right;
+			}
+
+			// Copy the rightmost node's value to the current node
+			current.key = rightMost.key;
+			current.value = rightMost.value;
+
+			// Eliminate the rightmost node
+			if (parentOfRightMost.right == rightMost) {
+				parentOfRightMost.right = rightMost.left;
+			} else {
+				parentOfRightMost.left = rightMost.left;
+			}
+		}
+
+		return removedV;
 	}
 
 	@Override
 	public int size() {
-		// TODO
-		return -1;
+		if (root == null) {
+			return 0;
+		}
+		return 1 + size(root.left) + size(root.right);
 	}
 
-	private class Node {
-		private K key;
-		private V value;
-		private Node left;
-		private Node right;
-
-		public Node(K key, V value) {
-			this.key = key;
-			this.value = value;
-			this.left = null;
-			this.right = null;
+	public int size(Node root) {
+		if (root == null) {
+			return 0;
 		}
-
-
+		return 1 + size(root.left) + size(root.right);
 	}
 
 }
